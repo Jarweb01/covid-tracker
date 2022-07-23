96f753b6ee72b7db7936ff53606ca72beb7ce3cf
Jarweb01 <jarvisweb@mail.ru>
2022-07-23 14:45:27 +0300

    Project started

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="text-center">
+    <h2 class="text-3xl font-bold">{{ text }}</h2>
+    <div class="text-2xl mt-4 mb-10">
+      {{ timestamp }}
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+// import func from "vue-editor-bridge";
+
+export default {
+  name: "DataTitle",
+  props: {
+    text: {
+      type: String,
+      required: false,
+      default: "false",
+    },
+    dataDate: {
+      type: String,
+      required: false,
+      default: "false",
+    },
+  },
+  computed: {
+    timestamp() {
+      return moment(this.dataDate).format("MMMM Do YYYY, h:mm:ss a");
+    },
+  },
+};
+</script>
